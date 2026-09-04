c7fe78a719e8c8ae74725dd8f3383577d571a5f9
Augusto Alvarez <augustoalvarez10@gmail.com>
2026-09-04 20:50:33 -0300

    Simplifica landing y mejora nitidez del hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import { Banner } from './components/Banner';
-import { Credenciales } from './components/Credenciales';
-import { Unidades } from './components/Unidades';
 import { Formulario } from './components/Formulario';
 import { Cierre } from './components/Cierre';
 import { PieDePagina } from './components/PieDePagina';
@@ -21,9 +19,6 @@ export default function App() {
       <Banner />
 
       <main>
-        <Credenciales />
-        <Unidades />
-
         {/* El titulo vive dentro de Formulario, no aca: al pasar a exito tiene
             que irse con el resto. Si no, la tarjeta queda diciendo "Dejanos tus
             datos" arriba de un "¡Listo!" y con dos h2 en la misma seccion. */}
